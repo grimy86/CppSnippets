6eee1872330f943318928c23ab3584f45b9715c2
grimy86 <grimy86@proton.me>
2025-04-30 11:28:06 +0200

    Post-Const

--- ConsoleApp/src/Const.cpp
+++ ConsoleApp/src/Const.cpp
@@ -4,19 +4,60 @@ namespace CONSTSPACE
 {
 #pragma region CONSTANTS
 	/*
-	*	Const is more like a promise to the compiler that you have to keep. It just means, this code won't change.
+	*	They keyword const marks an object as immutable.
+	*	It's a promise to the compiler that the data is readonly.
+	*	
+	*	Const correctness is the practice of using this mark consistently.
+	*		- Prevent accidental modification of data
+	*		- Enable safer and more optimized code
 	*/
 
-	void Const()
+	class ConstClass
 	{
-		const int MAX_AGE{ 90 };
-		
-		int* a = new int;
+	public:
+		void exampleFunc()
+		{
+			// VARIABLES
+			int var = 5;
+			const int x = 5;
+			// x = 6;, illegal because x is immutable
 
-		*a = 2;
-		a = (int*)&MAX_AGE;
 
-		std::cout << *a << std::endl;
-	}
+			// POINTERS
+			// pointer to const int (data is const, pointer is not)
+			// This is logical because x is const, we can't do: int* ptr = &x;
+			// int* ptr = &x; // illegal, x is const
+			const int* ptr1 = &x;
+			int const* ptr0 = &x; // same as above, just different syntax
+			// const pointer to int (pointer is const, data is not)
+			int* const ptr2 = &var;
+			// const pointer to const int
+			const int* const ptr3 = &x;
+			// what NOT TO DO, bypass constness
+			int* ptr{ (int*)&x };
+		}
+
+		// PARAMETERS
+		// Avoids unnecessary copies and enforces immutability.
+		void printValue(const std::string& str) { return; }
+
+		// Returning const values can prevent modification
+		const std::string getName() { return "myName"; }
+
+		// A method that cannot modify any class-members
+		// It won't change the object and is only a readonly method.
+		int getValue() const { return 0; }
+
+		// EXAMPLE
+		const int* const GetX() const
+		{
+			m_value = 5;
+			return m_x;
+		}
+
+	private:
+		mutable int m_value{ 5 }; // mutable allows modification in const methods
+		int* m_x, *m_y;
+	};
 #pragma endregion
 }
