dab317acd8728f0844e133edc3977d83d01a4b34
grimy86 <grimy86@proton.me>
2025-05-12 10:44:17 +0200

    Fix

--- ConsoleApp/src/Template_Functions.cpp
+++ ConsoleApp/src/Template_Functions.cpp
@@ -2,12 +2,23 @@
 
 namespace templatefunctions
 {
-	class templateExamples
+	class templates
 	{
+		/*
+		*	Get the compiler to write code for me based on the rules I've given it.
+		*	It like a blueprint.
+		*/
+
 		template<typename T>
 		void Print(T value)
 		{
 			std::cout << value << std::endl;
+		}
+
+		void Templates()
+		{
+			Print(5); // The compiler figures it out
+			Print<int>(5); // Explicit type passing
 		}
 	};
 }
